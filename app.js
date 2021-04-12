5511efc513a6f88e5da72365ce21b91033093d79
Bailey Kursar <bkursar@gmail.com>
2021-04-12 15:46:04 +0100

    Added bootstrap

--- app.js
+++ app.js
@@ -1,24 +1,15 @@
-/**
-* Required External Modules
-*/
+/* Required External Modules */
 const express = require("express");
 const path = require("path");
 
-/**
-* App Variables
-*/
+/* App Variables */
 const app = express();
 const port = process.env.PORT || "8000";
 
-/**
-*  App Configuration
-*/
+/* App Configuration */
 app.use(express.static(__dirname + '/public'));
 
-
-/**
-* Server Activation
-*/
+/* Server Activation */
 app.listen(port, () => {
   console.log(`Listening to requests on http://localhost:${port}`);
 });
