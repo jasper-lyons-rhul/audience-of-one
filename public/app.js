9f190ed9a251b0807d6074951c7f177240f5541d
Bailey Kursar <bkursar@gmail.com>
2021-04-12 18:08:16 +0100

    Adding firebase directly into app.js

--- public/app.js
+++ public/app.js
@@ -1,10 +1,23 @@
 /* Required External Modules */
 const express = require("express");
 const path = require("path");
+import firebase from "firebase/app";
+import "firebase/analytics";
 
 /* App Variables */
 const app = express();
 const port = process.env.PORT || "8000";
+cconst firebaseConfig = {
+  apiKey: "AIzaSyAhpWYBvDu_DF3HGig3Absum-qzKnOmm3k",
+  authDomain: "audience-of-one.firebaseapp.com",
+  projectId: "audience-of-one",
+  storageBucket: "audience-of-one.appspot.com",
+  messagingSenderId: "164576457461",
+  appId: "1:164576457461:web:037e8f768505b3bb404bbc",
+  measurementId: "G-521053G3PN"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 /* App and route configuration */
 app.set('view engine', 'ejs');
