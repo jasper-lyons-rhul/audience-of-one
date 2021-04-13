42be80c48e4de565ec99ead2a85fb2455730baf8
Bailey Kursar <bkursar@gmail.com>
2021-04-13 11:20:02 +0100

    taking out fs since don't think i need to stream

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const fs = require('fs');
 
 /* App variables */
 const app = express();
